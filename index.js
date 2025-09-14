af5244ebcc5b86110cdfa5c4ff40799ed1163d38
GonzaSai <136342939+GonzaSai@users.noreply.github.com>
2025-09-14 13:05:59 -0300

    tpAPIS

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ botonDia.onclick = function() {
 //Foto fecha específica
 let tituloFecha = document.querySelector('#tituloFecha');
 let contenedorFecha = document.querySelector('#contenedorFecha');
-let imagenFecha = document.querySelector('#imgFecha');
 let botonFecha = document.querySelector('#btnFecha');
 let inputFecha = document.querySelector('#inputFecha');
 botonFecha.onclick = function() {
@@ -26,8 +25,13 @@ botonFecha.onclick = function() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${inputFecha.value}`)
     .then(res => res.json())
     .then(fotoFecha => {
-        imagenFecha.src = fotoFecha.hdurl;
-        tituloFecha.textContent = fotoFecha.title;
-        contenedorFecha.innerHTML = `${contenedorFecha.innerHTML} <p>${fotoFecha.explanation}</p>`;
+        if (fotoFecha.code) {
+            // Si hay un error, mostrar mensaje
+            tituloFecha.textContent = "Error";
+            contenedorFecha.innerHTML = `<p>Fecha inválida o fuera de rango. Intenta con otra fecha.</p>`;
+        } else {
+            tituloFecha.textContent = fotoFecha.title;
+            contenedorFecha.innerHTML = `<img id="imgFecha" src="${fotoFecha.hdurl}" alt=""> <p>${fotoFecha.explanation}</p>`;
+        }
     });
 }
